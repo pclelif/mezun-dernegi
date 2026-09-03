0c1842ef0dd623653995f82d5b5ba782a3feaf7c
Elif Poçulu <pcl.elif@gmail.com>
2026-09-03 20:09:08 +0300

    Unify favicon sources for Google search

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,23 +12,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ec1c24",
     icons: [
       {
-        src: "/icon-48.png",
+        src: "/favicon-48.png",
         sizes: "48x48",
-        type: "image/png",
-      },
-      {
-        src: "/icon-96.png",
-        sizes: "96x96",
-        type: "image/png",
-      },
-      {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
         type: "image/png",
       },
     ],
